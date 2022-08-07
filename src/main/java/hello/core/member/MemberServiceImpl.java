69e9be2caae855c51b9af23b8220d45709fc317e
jeehyunkuen <jeehk1108@naver.com>
2022-08-07 09:01:06 +0900

    first commit

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    //생성자로 관심사를 분리하여 주입 (생성자 주입)
+    @Autowired
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository =  memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
